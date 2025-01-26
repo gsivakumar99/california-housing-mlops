8c9b6e52a11f3ede175f2830984159484334e862
gsivakumar99 <2023aa05486@wilp.bits-pilani.ac.in>
2025-01-26 17:25:15 +0000

    Modified the training file to include 3 different algorithms

--- src/train.py
+++ src/train.py
@@ -1,14 +1,13 @@
 import os
-from datetime import datetime
 
 import mlflow
 from mlflow.models.signature import infer_signature
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.tree import DecisionTreeRegressor
 
-from data import load_data, preprocess_data
+from src.data import load_data, preprocess_data
 
 # Create model directory
 MODEL_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models')
@@ -23,8 +22,10 @@ def train_and_log_model(model, model_name, X_train, X_test, y_train, y_test):
         y_test_pred = model.predict(X_test)
 
         # Calculate and log metrics
-        mlflow.log_metric("train_mse", mean_squared_error(y_train, y_train_pred))
-        mlflow.log_metric("test_mse", mean_squared_error(y_test, y_test_pred))
+        mlflow.log_metric("train_mse",
+                          mean_squared_error(y_train, y_train_pred))
+        mlflow.log_metric("test_mse",
+                          mean_squared_error(y_test, y_test_pred))
         mlflow.log_metric("test_r2", r2_score(y_test, y_test_pred))
 
         # Infer the model signature
@@ -46,7 +47,7 @@ def train_and_log_model(model, model_name, X_train, X_test, y_train, y_test):
 
 def train_models():
     mlflow.set_experiment("california_housing")
-    
+
     # Load and preprocess data
     df = load_data()
     X_train, X_test, y_train, y_test = preprocess_data(df)
@@ -55,11 +56,13 @@ def train_models():
     models = [
         ("Linear Regression", LinearRegression()),
         ("Decision Tree Regressor", DecisionTreeRegressor(max_depth=5)),
-        ("Random Forest Regressor", RandomForestRegressor(n_estimators=50, max_depth=10))
+        ("Random Forest Regressor",
+         RandomForestRegressor(n_estimators=50, max_depth=10))
     ]
 
     for model_name, model in models:
-        train_and_log_model(model, model_name, X_train, X_test, y_train, y_test)
+        train_and_log_model(model, model_name, X_train,
+                            X_test, y_train, y_test)
 
 
 if __name__ == "__main__":
