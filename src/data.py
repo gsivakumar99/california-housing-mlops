d9445992c26869fafb496d1d736472ca7d12c4ba
gsivakumar99 <2023aa05486@wilp.bits-pilani.ac.in>
2025-01-27 10:51:25 +0000

    linting changes

--- src/data.py
+++ src/data.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def load_data(file_name=r'./data/california_housing.csv'):
+
+def load_data(file_name=r"./data/california_housing.csv"):
     # Load California Housing dataset from CSV
     df = pd.read_csv(file_name)
     return df
 
+
 def preprocess_data(df):
-    X = df.drop('PRICE', axis=1)
-    y = df['PRICE']
+    X = df.drop("PRICE", axis=1)
+    y = df["PRICE"]
     return train_test_split(X, y, test_size=0.2, random_state=42)
